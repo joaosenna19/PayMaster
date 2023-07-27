51f8506752c82ee9f95e7f14851612af86aaf9eb
João Senna Radicchi <joaoradicchi@gmail.com>
2023-07-27 12:55:10 -0600

    Add Employee Class and its children

---
+++ PayMaster/Data/HourlyEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace PayMaster.Data;
+
+public class HourlyEmployee : Employee
+{
+    private double HourlyRate { get; set; }
+    
+    public HourlyEmployee(int id, string firstName, string lastName, string position, bool isActive, DateTime dateHired, double hourlyRate) 
+        : base(id, firstName, lastName, position, isActive, dateHired)
+    {
+        HourlyRate = hourlyRate;
+    }
+    
+}
---
+++ PayMaster/Data/SalaryEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace PayMaster.Data;
+
+public class SalaryEmployee : Employee
+{
+    
+    private double Salary { get; set; }
+    
+    public SalaryEmployee(int id, string firstName, string lastName, string position, bool isActive, DateTime dateHired, double salary) 
+        : base(id, firstName, lastName, position, isActive, dateHired)
+    {
+        Salary = salary;
+    }
+}
